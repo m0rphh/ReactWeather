5dd0e3dee236065db74d98101b22565ac92f1767
Goran Matejin <morph@Gorans-MacBook-Pro.local>
2017-01-27 01:03:30 +0100

    ba

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './app/app.jsx', //fajl koji webpack učitava
+  entry: './app/app.jsx',
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -7,30 +7,27 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-        Main: 'app/components/Main.jsx',
-        Nav: 'app/components/Nav.jsx',
-        Weather: 'app/components/Weather.jsx',
-        WeatherForm: 'app/components/WeatherForm.jsx',
-        WeatherMessage: 'app/components/WeatherMessage.jsx',
-        About: 'app/components/About.jsx',
-        Examples: 'app/components/Examples.jsx',
-        openWeatherMap: 'app/API/openWeatherMap.jsx',
-
-
-
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      Weather: 'app/components/Weather.jsx',
+      WeatherForm: 'app/components/WeatherForm.jsx',
+      WeatherMessage: 'app/components/WeatherMessage.jsx',
+      About: 'app/components/About.jsx',
+      Examples: 'app/components/Examples.jsx',
+      openWeatherMap: 'app/api/openWeatherMap.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [ //loader služi da bi webpack znao šta da radi sa fajom
-       {
-         loader: 'babel-loader', //loader
-         query: {
-           presets: ['react', 'es2015'] //kompajlira react
-         },
-         test: /\.jsx?$/, //ovde kažemo da traži fajlove sa jsx ekstenzijom
-         exclude: /(node_modules|bower_components)/ //isključimo foldere koji nam ne trebaju
-       }
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
     ]
   },
   devtool: 'cheap-module-eval-source-map'
