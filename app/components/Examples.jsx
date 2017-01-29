6a339978cda7b7b4e9774c90d4859e7ea8079d1d
Goran Matejin <morph@Gorans-MacBook-Pro.local>
2017-01-29 22:07:11 +0100

    First styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are some examples to try out:</p>
+      <ol>
+        <li>
+          <Link todo='/?Location=Belgrade'>Beograd, RS</Link>
+        </li>
+        <li>
+          <Link todo='/?Location=Kragujevac'>Kragujevac, RS</Link>
+        </li>
+      </ol>
     </div>
   )
 };
