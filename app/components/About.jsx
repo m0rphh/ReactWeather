8d0bb3384ff7e495e4c7c7e7c696b3ad1c433d75
Goran Matejin <morph@Gorans-MacBook-Pro.local>
2017-01-29 18:27:10 +0100

    a

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,10 +2,7 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
-    </div>
+    <h3>About Component</h3>
   )
 };
 
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,11 @@
 var React = require('react');
 
-var Examples = (props) => {
-  return (
-    <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
-    </div>
-  )
-};
+var Examples = React.createClass({
+  render: function () {
+    return (
+      <h3>Examples Component</h3>
+    );
+  }
+});
 
 module.exports = Examples;
