11303190d39ba7876f38d706b5e8383e408686f3
Goran Matejin <morph@Gorans-MacBook-Pro.local>
2017-02-03 11:51:24 +0100

    City examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,14 +4,15 @@ var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h1 className="text-center">Examples</h1>
-      <p>Here are some examples to try out:</p>
+      <h1 className="text-center page-title">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
-          <Link todo='/?Location=beograd'>Beograd</Link>
+            <Link to='/?location=Belgrade'>Belgrade, Serbia</Link>
+
         </li>
         <li>
-          <Link todo='/?Location=Kragujevac'>Kragujevac</Link>
+          <Link to='/?location=Kragujevac'>Kragujevac, Serbia</Link>
         </li>
       </ol>
     </div>
